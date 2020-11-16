ee0400e8c52ac87027f6347723d98925df756c05
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-16 10:46:38 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+namespace _26.debugExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sum = CalcSum(3, 4, 5);
+            Console.WriteLine(sum);
+
+        }
+        private static int CalcSum(int a, int b, int c) 
+        {
+            
+            var partialSum = Sum(a, b);
+            var totalSum = Sum(partialSum, c);
+            return totalSum;
+        }
+        private static int Sum(int a, int b) 
+        {
+            
+            return a + b;
+        }
+    }
+}
